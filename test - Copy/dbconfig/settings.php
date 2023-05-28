4a72985434f360fbc9c6463cd663021d7e639900
newbee1905 <beenewminh@outlook.com>
2023-05-29 04:47:43 +1000

    chore: clean up files

--- test - Copy/dbconfig/settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("HOST", "localhost:3307");
-define("DATABASE", "assignmentjavastorm");
-define("USERNAME", "root");
-define("PASSWORD", "Mo!ConCho2003");
-?>
--- test - Copy/settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$host = "localhost";
-	$user = "kwen"; // your user name
-	$pwd = "Loey5312126@"; // your password (date of birth ddmmyy unless changed)
-	$sql_db = "COS10026"; // your database
-?>
-	
-	
-	
