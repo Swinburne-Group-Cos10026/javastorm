35593cf930f61f3b4b4af88bc672ac1e74986644
newbee1905 <beenewminh@outlook.com>
2023-05-30 06:48:50 +1000

    feat: add default sql

--- dbconfig/settings.php
+++ dbconfig/settings.php
@@ -1,10 +1,5 @@
 <?php
-	// $host = "feenix-mariadb.swin.edu.au";
-	// $user = "s103809103"; // your user name
-	// $pwd = "021103"; // your password (date of birth ddmmyy unless changed)
-	// $sql_db = "s103809103_db"; // your database
-	
-	$host = "localhost";
-	$user = "root"; // your user name
-	$pwd = "root"; // your password (date of birth ddmmyy unless changed)
-	$sql_db = "ass2"; // your database
+	$host = "feenix-mariadb.swin.edu.au";
+	$user = "s103809103"; // your user name
+	$pwd = "021103"; // your password (date of birth ddmmyy unless changed)
+	$sql_db = "s103809103_db"; // your database
